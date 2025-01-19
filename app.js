7ce75887fa58246da3b15e6b6ee4b9729d8c6be6
m <m>
2025-01-19 21:27:37 +0500

    qoshildi

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const loginButton = document.querySelecxtor('.btn1');
+const loginButton = document.querySelector('.btn1');
 loginButton.addEventListener('click', (event) => {
     event.preventDefault();
     window.location.href = "home.html";
 });
 
-
 const signUPbutton = document.querySelector('.btn2');
-
 signUPbutton.addEventListener('click', (eventDef) => {
     eventDef.preventDefault();
     window.location.href = "home.html";
 });
-
-    
